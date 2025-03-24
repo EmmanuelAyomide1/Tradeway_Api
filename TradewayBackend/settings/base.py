dfc87aabaf4da1c0fedc02e2acf24e011b43571d
Osigelialex <osigelialex@gmail.com>
2025-03-24 18:27:49 +0100

    feat: add token blacklist support to Django REST framework

--- TradewayBackend/settings/base.py
+++ TradewayBackend/settings/base.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     "rest_framework.authtoken",
+    'rest_framework_simplejwt.token_blacklist',
     "corsheaders",
     'drf_yasg',
 
