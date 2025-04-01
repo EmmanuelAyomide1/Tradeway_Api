b345b64a5027fc21f9a842199b26ada52b9a4486
unknown <owoeyeemmanuel206@gmail.com>
2025-04-01 07:37:28 +0100

    ft(products):created all required endpoint for products

--- product/apps.py
+++ product/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
 
-class AccountConfig(AppConfig):
+class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product'
+
+    def ready(self):
+        from product import signals
--- product/views.py
+++ product/views.py
@@ -1,24 +1,27 @@
-from operator import is_
-import cloudinary
-
 from django.db.models import Count
 
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, mixins
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from TradewayBackend.pagination import CustomPagination
+from product.utils import deleteImageInCloudinary
 
-from .models import Category,ProductReview, Product
-from .permissions import IsAdmin
+from .models import Category, ProductImage, ProductReview, Product, SavedProduct
 from .serializers import (
     CategorySerializer,
-    CategoryUpdateSerializer, 
-    ProductReviewSerializer, 
-    ProductReviewListSerializer
-    )
-from .utils import IsReviewOwnerOrAdminPermission
+    CategoryUpdateSerializer,
+    ProductDetailSerializer,
+    ProductImageSerializer,
+    ProductImageUpdateSerializer,
+    ProductReviewSerializer,
+    ProductReviewListSerializer,
+    ProductSerializer,
+    ProductUpdateSerializer,
+    SavedProductSerializer
+)
+from .permissions import IsProductSeller, IsReviewOwnerOrAdminPermission, IsAdmin, IsSellerOrReadOnly
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -43,12 +46,12 @@ class CategoryViewSet(viewsets.ModelViewSet):
         else:
             permission_classes = [IsAuthenticated, IsAdmin]
         return [permission() for permission in permission_classes]
-    
+
     def get_serializer_class(self):
         if self.action in ['update', 'partial_update']:
-            return CategoryUpdateSerializer  
+            return CategoryUpdateSerializer
         return super().get_serializer_class()
-        
+
     def update(self, request, *args, **kwargs):
         """
         Update a category (admin only)
@@ -57,15 +60,11 @@ class CategoryViewSet(viewsets.ModelViewSet):
         old_image = instance.image
         serializer = self.get_serializer(instance, data=request.data)
         serializer.is_valid(raise_exception=True)
-        
+
         # If image is being updated, delete the old image from Cloudinary
         if old_image and 'image' in request.data and old_image != request.data['image']:
-            try:
-                cloudinary.uploader.destroy(old_image.name)
-            except Exception as e:
-                # Log error but continue with update
-                print(f"Error deleting old image: {e}")
-        
+            deleteImageInCloudinary(old_image)
+
         self.perform_update(serializer)
         return Response(serializer.data)
 
@@ -75,30 +74,86 @@ class ProductReviewViewSet(viewsets.ModelViewSet):
     permission_classes = [IsReviewOwnerOrAdminPermission]
     queryset = ProductReview.objects.all()
     pagination_class = CustomPagination
-    
+
     def get_queryset(self):
         queryset = ProductReview.objects.all().exclude(is_offensive=True)
-        
-       
+
         product_id = self.request.query_params.get('product_id')
         if product_id:
             queryset = queryset.filter(product_id=product_id)
-        
 
         rating = self.request.query_params.get('rating')
         if rating:
             queryset = queryset.filter(rating=rating)
-        
+
         # Sorting
         sort = self.request.query_params.get('sort', 'newest')
         if sort == 'newest':
             queryset = queryset.order_by('-created_at')
         elif sort == 'oldest':
             queryset = queryset.order_by('created_at')
-        
+
         return queryset
-    
+
     def get_serializer_class(self):
         if self.action in ['retrieve', 'list']:
             return ProductReviewListSerializer
         return super().get_serializer_class()
+
+
+class ProductViewset(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all().order_by('created_at')
+    pagination_class = CustomPagination
+    permission_classes = [IsSellerOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["id", "name", "categories__name", "categories__id"]
+    http_method_names = ["get", "post", "patch", "delete"]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def get_permissions(self):
+        """
+        Return permissions based on action:
+        - List and retrieve: allow all users
+        - Create, update, delete: admin only
+        """
+        if self.action in ['update', 'partial_update', 'destroy']:
+            permission_classes = [IsProductSeller]
+        else:
+            permission_classes = self.permission_classes
+        return [permission() for permission in permission_classes]
+
+    def get_serializer_class(self):
+        if self.action in ['update', 'partial_update']:
+            return ProductUpdateSerializer
+        elif self.action == 'retrieve':
+            return ProductDetailSerializer
+        return super().get_serializer_class()
+
+
+class ProductImageViewset(viewsets.ModelViewSet):
+    serializer_class = ProductImageSerializer
+    queryset = ProductImage.objects.all()
+    parser_classes = [MultiPartParser, FormParser]
+    permission_classes = [IsProductSeller]
+    http_method_names = ["post", "patch", "delete"]
+
+    def get_serializer_class(self):
+        if self.action in ['update', 'partial_update']:
+            return ProductImageUpdateSerializer
+        return super().get_serializer_class()
+
+
+class SavedProductViewset(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    serializer_class = SavedProductSerializer
+    queryset = SavedProduct.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["id", "product__name"]
+    permission_classes = [IsAuthenticated]
+    pagination_class = CustomPagination
+    http_method_names = ['post', 'delete', 'get']
